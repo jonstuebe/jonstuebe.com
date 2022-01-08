c95aa4e90d26b18f3cafdada606261836486fe64
Jon Stuebe <jstuebe@gmail.com>
2022-01-08 09:21:34 -0700

    feat: added music page to header nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,33 +1,40 @@
 import { Link } from "remix";
 
 export function Header() {
-	return (
-		<header className="flex justify-between items-center">
-			<Link className="text-white no-underline select-none" to="/">
-				<h2 className="">Jon Stuebe</h2>
-			</Link>
-			<nav className="flex space-x-4">
-				<Link
-					className="hidden md:inline-flex text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
-					to="/"
-				>
-					home
-				</Link>
+  return (
+    <header className="flex justify-between items-center">
+      <Link className="text-white no-underline select-none" to="/">
+        <h2 className="">Jon Stuebe</h2>
+      </Link>
+      <nav className="flex space-x-4">
+        <Link
+          className="hidden md:inline-flex text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
+          to="/"
+        >
+          home
+        </Link>
 
-				<Link
-					className="text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
-					to="/blog"
-				>
-					blog
-				</Link>
+        <Link
+          className="text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
+          to="/blog"
+        >
+          blog
+        </Link>
 
-				<Link
-					className="text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
-					to="/notes"
-				>
-					notes
-				</Link>
-			</nav>
-		</header>
-	);
+        <Link
+          className="text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
+          to="/notes"
+        >
+          notes
+        </Link>
+
+        <Link
+          className="text-base text-gray-400 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium no-underline select-none"
+          to="/music"
+        >
+          music
+        </Link>
+      </nav>
+    </header>
+  );
 }

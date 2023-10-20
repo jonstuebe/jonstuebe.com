4a0293474a01dd2025050dd1d49578a13fe1aa12
Jon Stuebe <jstuebe@gmail.com>
2023-10-20 15:23:31 -0700

    chore: move back to yarn

--- remix.config.js
+++ remix.config.js
@@ -12,12 +12,4 @@ module.exports = {
   appDirectory: "app",
   ignoredRouteFiles: [".*"],
   tailwind: true,
-  future: {
-    v2_routeConvention: true,
-    v2_errorBoundary: true,
-    v2_dev: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-  },
 };

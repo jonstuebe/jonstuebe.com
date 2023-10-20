ffcf9707a4b7208c99609d10835f620b5771b38d
Jon Stuebe <jstuebe@gmail.com>
2023-10-20 15:47:25 -0700

    chore: testing out streaming

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,39 @@
-import { renderToString } from "react-dom/server";
+import { handleRequest } from "@vercel/remix";
 import { RemixServer } from "@remix-run/react";
-import { type EntryContext } from "@vercel/remix";
+import type { EntryContext } from "@vercel/remix";
 
-export default function handleRequest(
+export default function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer
   );
-
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
 }
+// import { renderToString } from "react-dom/server";
+// import { RemixServer } from "@remix-run/react";
+// import { type EntryContext, handleRequest } from "@vercel/remix";
+
+// export default function handleRequest(
+//   request: Request,
+//   responseStatusCode: number,
+//   responseHeaders: Headers,
+//   remixContext: EntryContext
+// ) {
+//   const markup = renderToString(
+//     <RemixServer context={remixContext} url={request.url} />
+//   );
+
+//   responseHeaders.set("Content-Type", "text/html");
+
+//   return new Response("<!DOCTYPE html>" + markup, {
+//     status: responseStatusCode,
+//     headers: responseHeaders,
+//   });
+// }

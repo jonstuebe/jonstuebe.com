ccf1300c8dd2a6aa042d6c0abaffe4ca277b3540
Jon Stuebe <jstuebe@gmail.com>
2023-10-21 13:28:19 -0700

    feat: updated error boundary and added analytics

--- app/root.tsx
+++ app/root.tsx
@@ -49,6 +49,7 @@ export default function App() {
         <Links />
       </head>
       <body className="m-0 antialiased font-sans text-white scrollbar scrollbar-thumb-gray-700 scrollbar-track-gray-900 scrollbar-thumb-rounded-lg w-full h-full">
+        <Analytics />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -98,44 +99,33 @@ export function ErrorBoundary() {
   }
 
   return (
-    <div>
-      <h1>Uh oh ...</h1>
-      <p>Something went wrong.</p>
-    </div>
+    <html lang="en" className="overflow-y-scroll">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Error - Jon Stuebe</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="m-0 antialiased font-sans text-white scrollbar scrollbar-thumb-gray-700 scrollbar-track-gray-900 scrollbar-thumb-rounded-lg">
+        <Analytics />
+        <Layout>
+          <Header />
+          <main>
+            <div className="flex flex-row items-center justify-center py-40">
+              <h1 className="text-6xl tracking-tight motion-safe:animate-text-in-slow select-none">
+                Uh Oh
+              </h1>
+              <div className="h-32 mx-8 w-1 bg-white"></div>
+              <h2 className="text-6xl tracking-tight motion-safe:animate-text-in-slow select-none">
+                Something went wrong
+              </h2>
+            </div>
+          </main>
+          <Footer />
+        </Layout>
+        <Scripts />
+      </body>
+    </html>
   );
 }
-
-// export function CatchBoundary() {
-//   const caught = useCatch();
-//   return (
-//     <html lang="en" className="overflow-y-scroll">
-//       <head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width,initial-scale=1" />
-//         <title>Error - Jon Stuebe</title>
-//         <Meta />
-//         <Links />
-//       </head>
-//       <body className="m-0 antialiased font-sans text-white scrollbar scrollbar-thumb-gray-700 scrollbar-track-gray-900 scrollbar-thumb-rounded-lg">
-//         <Layout>
-//           <Header />
-//           <main>
-//             <div className="flex flex-row items-center justify-center py-40">
-//               <h1 className="text-6xl tracking-tight motion-safe:animate-text-in-slow select-none">
-//                 {caught.status}
-//               </h1>
-//               <div className="h-32 mx-8 w-1 bg-white"></div>
-//               <h2 className="text-6xl tracking-tight motion-safe:animate-text-in-slow select-none">
-//                 {caught.statusText}
-//               </h2>
-//             </div>
-//           </main>
-//           <Footer />
-//         </Layout>
-
-//         <Scripts />
-//         {process.env.NODE_ENV === "development" && <LiveReload />}
-//       </body>
-//     </html>
-//   );
-// }

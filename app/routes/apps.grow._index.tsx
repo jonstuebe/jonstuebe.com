431a990756d3e9153491320942b8dab3811120a5
Jon Stuebe <jstuebe@gmail.com>
2023-12-28 19:23:53 -0700

    feat: added grow app to site

--- app/routes/apps.grow._index.tsx
+++ app/routes/apps.grow._index.tsx
@@ -3,36 +3,36 @@ import { createCacheHeader } from "../utils/cache";
 
 export const headers = createCacheHeader({ stale: "1month" });
 
-export default function Quench() {
+export default function Grow() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 w-full h-full m-auto relative">
       <img
-        src="/apps/quench/logomark.svg"
-        width={40}
+        src="/apps/grow/logomark.svg"
+        width={60}
         className="motion-safe:animate-water-drop motion-safe:opacity-0 mb-2"
       />
       <h1 className="motion-safe:animate-fade-in-slow motion-safe:opacity-0 m-0 mb-2">
-        Quench
+        Grow
       </h1>
       <p className="text-center text-gray-400 text-md motion-safe:animate-fade-in-slow m-0 mb-8">
-        A simple water tracking app
+        An app to track your goals
       </p>
-      <a
-        href="https://apps.apple.com/us/app/quench-tracker/id1577232272"
+      {/* <a
+        href="https://apps.apple.com/us/app/grow-tracker/id1577232272"
         target="_blank"
         className="motion-safe:animate-fade-in-slow mb-4"
       >
         <img src="/app-store.svg" width={120} />
-      </a>
+      </a> */}
       <div className="flex flex-row justify-center gap-4 motion-safe:animate-fade-in-slow">
         <Link
-          to="/apps/quench/support"
+          to="/apps/grow/support"
           className="text-xs text-gray-500 no-underline"
         >
           support
         </Link>
         <Link
-          to="/apps/quench/privacy-policy"
+          to="/apps/grow/privacy-policy"
           className="text-xs text-gray-500 no-underline"
         >
           privacy policy
